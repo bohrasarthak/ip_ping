43271434ae6367bc94526314b99db2b9a16076b5
bohrasarthak <bohrasarthak@gmail.com>
2020-08-26 16:52:42 +0530

    First commit

---
+++ IP_Ping_multiple.ps1
@@ -0,0 +1,16 @@
+﻿$names = Get-content "C:\ip_names.txt"
+foreach ($name in $names){
+  if (Test-Connection -ComputerName $name -Count 1 -ErrorAction SilentlyContinue){
+   $Output = "$name is up"+"`n"
+   Write-Host "$name is up" -ForegroundColor Green
+  }
+  else{
+    $Output = "$name is down,"+"`n" 
+    Write-Host "$name is down" -ForegroundColor Red
+  }
+
+$Output | Out-file "C:\$name.txt"
+
+}
+
+Start-Sleep -s 15
---
+++ IP_Ping_single.ps1
@@ -0,0 +1,14 @@
+﻿$names = Get-content "C:\ip_names.txt"
+foreach ($name in $names){
+  if (Test-Connection -ComputerName $name -Count 1 -ErrorAction SilentlyContinue){
+   $Output+= "$name is up,"+"`n"
+   Write-Host "$name is up" -ForegroundColor Green
+  }
+  else{
+    $Output+= "$name is down,"+"`n" 
+    Write-Host "$name is down" -ForegroundColor Red
+  }
+}
+
+$Output | Out-file "C:\result.txt"
+Start-Sleep -s 15
